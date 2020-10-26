63980267e4e2df209cc9ea92a2156b64fcc7826b
Nolaine Crusher <nolainecrusher@gmail.com>
2020-10-26 13:12:29 -0700

    trying to troubleshoot a date that isn't being caught

--- Sources/SwiftTaggerMP4/ConvenienceExtensions/String.swift
+++ Sources/SwiftTaggerMP4/ConvenienceExtensions/String.swift
@@ -12,7 +12,7 @@ extension String {
         let isoFormatter = ISO8601DateFormatter()
         isoFormatter.formatOptions = [.withInternetDateTime]
         isoFormatter.timeZone = TimeZone(secondsFromGMT: 0) ?? .current
-        
+        print("SOURCE STRING: \(self)")
         let formats: [String] = ["d MMM yyyy HH:mm:ss", "yyyy-MM-ddTHH:mm", "yyyy-MM-ddTHH:mm:ssZ", "MM-dd-yyyy HH:mm","yyyy-MM-ddTHH", "MMM d, yyyy", "d MMM yyyy", "yyyy-MM-dd", "MM/dd/yyyy", "dd/MM/yyyy", "dd.MM.yy", "dd.MM.yyyy", "dd-MM-yy", "dd-MM-yyyy", "dd-MMM-yyyy", "dd-MMM-yy", "MM-dd-yyyy", "MMMM yyyy", "yyyy-MM", "yyyy"]
         let dateFormatters: [DateFormatter] = formats.map { format in
             let formatter = DateFormatter()
@@ -26,6 +26,7 @@ extension String {
         } else {
             for format in dateFormatters {
                 if let date = format.date(from: self) {
+                    print("OUTPUT DATE: \(date)")
                     return date
                 } else {
                     return nil
